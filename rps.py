11aa136c7439ad6ba34ce1a46663005ff550981c
Marco <>
2021-06-22 16:33:31 -0700

    upload rps.py

---
+++ rps.py
@@ -0,0 +1,12 @@
+#while(user wants to keep playing)
+#{
+#output selections available to player
+#take input from player: 1=Rock, 2=Paper, 3=Scissors
+#randomly generate a number from (1, 3)
+#compare user input and bot generated number
+#output user and bot choices, and who won (or if it is a tie)
+#Ask user if they wish to continue
+#}
+#if user wishes to not continue, display number of times player won, lost, and tied. 
+#When player stops playing display number of times user chose rock, paper, and scissors
+#After game ends, tell player which single object would have had the max number of wins against bot
